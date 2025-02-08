489ad5fe951add8957504f20d52ae6f6ddd05df4
HeckerWithAHat <aryanj1010@gmail.com>
2025-02-07 19:39:21 -0800

    commit

--- OS.py
+++ OS.py
@@ -4,6 +4,7 @@ import time
 import epd2in13b_V4
 import globalvars
 from gpiozero import *
+import nmcli
 class OS:
     def __init__(self):
         # If yes do all the wifi stuff, and set the wifi variable in global vars to true
@@ -25,107 +26,93 @@ class OS:
                 usingWifi = False
                 break
         globalvars.wifiModeActive = usingWifi
+        didWifiConnect = False
         if globalvars.wifiModeActive:
-            didWifiConnect = False
-            while didWifiConnect == False:
-                wifi_SSID = ""
-                lines = subprocess.check_output(["iwlist", "wlan0", "scan"]).decode("utf-8").split('\n')
-                ssids = set()
-                for line in lines:
-                    if "ESSID:" in line:
-                        ssid = line.split("ESSID:")[-1].strip()
-                        ssids.add(ssid)
-                ssids = sorted(list(ssids))
-                EPDAPI.createImageFromOptions("What is the name of the WiFi?", ssids)
-                while wifi_SSID == "":
-                    if globalvars.buttons["IM"].is_pressed: # left
-                        ssids.append(ssids.pop(0))
-                            
-                    if globalvars.buttons["MM"].is_pressed: # select
-                        wifi_SSID = ssids[0]
-                        break
-                    if globalvars.buttons["RM"].is_pressed: # right
-                        ssids.insert(0,ssids.pop())
-                wifi_PASS = ""
-                currentPlace = (0,0)
-                charList = [list('ABCDEFGHIJKLMNOPQRS'),list('TUVWXYZabcdefghijkl'),list('mnopqrstuvwxyz01234'),list('56789!@#$%^&*()-_+='),list('~`[]{}|\\:;"\'<>,.?/ ')]
-                EPDAPI.createKeyboardFromPrompt("What is the password?", charList[currentPlace[0]][currentPlace[1]])
-                # read input and update the currentChar, then when select is pressed, add current char to wifi_PASS
-                currenttext = ""
-                while wifi_PASS == "":
-                    if globalvars.buttons["IM"].is_pressed: # left
-                        currentPlace = (currentPlace[0], (currentPlace[1] - 1) % 19)
-                        print(currentPlace)
-                        EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
-                    if globalvars.buttons["MM"].is_pressed: # select
-                        currenttext +=charList[currentPlace[0]][currentPlace[1]]
-                        EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
-                    if globalvars.buttons["RM"].is_pressed: # right
-                        currentPlace = (currentPlace[0], (currentPlace[1] + 1) % 19)
-                        print(currentPlace)
-                        EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
-                    if globalvars.buttons["MT"].is_pressed: # up
-                        currentPlace = ((currentPlace[0] - 1) % 5, currentPlace[1])
-                        print(currentPlace)
-                        EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
-                    if globalvars.buttons["MB"].is_pressed: # down
-                        currentPlace = ((currentPlace[0] + 1) % 5, currentPlace[1])
-                        print(currentPlace)
-                        EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
-                    if globalvars.buttons["IT"].is_pressed: # YES
-                        wifi_PASS = currenttext
-                        break
-                    if globalvars.buttons["RT"].is_pressed: # back
-                        currenttext = currenttext[:-1] 
-                        EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
+            changeWifi = None
+            EPDAPI.createImageFromOptions("Do you want to change WiFi?", ["Yes", "No"])
+            while changeWifi == None:
+                if globalvars.buttons["IT"].is_pressed: # YES
+                    changeWifi = True
+                if globalvars.buttons["RT"].is_pressed: # NO
+                    changeWifi = False
+                    break
+            if changeWifi:
+                while didWifiConnect == False:
+                    wifi_SSID = ""
+                    lines = subprocess.check_output(["iwlist", "wlan0", "scan"]).decode("utf-8").split('\n')
+                    print(lines)
+                    ssids = set()
+                    for line in lines:
+                        if "ESSID:" in line:
+                            ssid = line.split("ESSID:")[-1].strip()
+                            ssids.add(ssid)
+                    ssids = sorted(list(ssids))
+                    print(ssids)
+                    EPDAPI.createImageFromOptions("What is the name of the WiFi?", ssids)
+                    while wifi_SSID == "":
+                        if globalvars.buttons["IM"].is_pressed: # left
+                            ssids.append(ssids.pop(0))
+                                
+                        if globalvars.buttons["MM"].is_pressed: # select
+                            wifi_SSID = ssids[0]
+                            break
+                        if globalvars.buttons["RM"].is_pressed: # right
+                            ssids.insert(0,ssids.pop())
+                    wifi_PASS = ""
+                    currentPlace = (0,0)
+                    charList = [list('ABCDEFGHIJKLMNOPQRS'),list('TUVWXYZabcdefghijkl'),list('mnopqrstuvwxyz01234'),list('56789!@#$%^&*()-_+='),list('~`[]{}|\\:;"\'<>,.?/ ')]
+                    EPDAPI.createKeyboardFromPrompt("What is the password?", charList[currentPlace[0]][currentPlace[1]])
+                    # read input and update the currentChar, then when select is pressed, add current char to wifi_PASS
+                    currenttext = ""
+                    while wifi_PASS == "":
+                        if globalvars.buttons["IM"].is_pressed: # left
+                            currentPlace = (currentPlace[0], (currentPlace[1] - 1) % 19)
+                            print(currentPlace)
+                            EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
+                        if globalvars.buttons["MM"].is_pressed: # select
+                            currenttext +=charList[currentPlace[0]][currentPlace[1]]
+                            EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
+                        if globalvars.buttons["RM"].is_pressed: # right
+                            currentPlace = (currentPlace[0], (currentPlace[1] + 1) % 19)
+                            print(currentPlace)
+                            EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
+                        if globalvars.buttons["MT"].is_pressed: # up
+                            currentPlace = ((currentPlace[0] - 1) % 5, currentPlace[1])
+                            print(currentPlace)
+                            EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
+                        if globalvars.buttons["MB"].is_pressed: # down
+                            currentPlace = ((currentPlace[0] + 1) % 5, currentPlace[1])
+                            print(currentPlace)
+                            EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
+                        if globalvars.buttons["IT"].is_pressed: # YES
+                            wifi_PASS = currenttext
+                            break
+                        if globalvars.buttons["RT"].is_pressed: # back
+                            currenttext = currenttext[:-1] 
+                            EPDAPI.createKeyboardFromPrompt("What is the password: " + currenttext, charList[currentPlace[0]][currentPlace[1]])
 
-                self.changeWifi(wifi_SSID, wifi_PASS)
-                didWifiConnect = self.connectToWiFi()
+                    self.changeWifi(wifi_SSID, wifi_PASS)
+                    didWifiConnect = self.connectedToWiFi()
+            else:
+                didWifiConnect = self.connectedToWiFi()
     
         
     def changeWifi(self, name, password):
-        # Read the current wpa_supplicant.conf file
-        config_file = '/etc/wpa_supplicant/wpa_supplicant.conf'
+        # Disconnect from current network
+        nmcli.radio.wifi_off()
+        nmcli.radio.wifi_on()
+        newWifi = True  
+        conns = nmcli.connection()
+        for conn in conns:
+            if conn.name == name:
+                newWifi = False
+                break
+        if newWifi:
+            nmcli.connection.up(name)
+        else:
+            nmcli.device.wifi_connect(name, password)
         
-        # Create a temporary file to store the modified configuration
-        temp_file = f'/tmp/{os.getpid()}_wpa_supplicant.conf'
-        
-        try:
-            # Read the existing configuration
-            with open(config_file, 'r') as f:
-                content = f.read()
-            
-            # Parse the content to find the last network block
-            lines = content.split('\n')
-            last_network_index = None
-            for i, line in enumerate(lines):
-                if line.startswith('network={'):
-                    last_network_index = i
-                    break
-            
-            # Add the new network block after the last existing one
-            if last_network_index is not None:
-                lines.insert(last_network_index + 1, '\n')
-                lines.insert(last_network_index + 2, f'network={{\n')
-                lines.insert(last_network_index + 3, f'    ssid="{name}"\n')
-                lines.insert(last_network_index + 4, f'    psk="{password}"\n')
-                lines.insert(last_network_index + 5, '}}\n')
-            
-            # Write the modified content to the temporary file
-            with open(temp_file, 'w') as f:
-                f.writelines(lines)
-            
-            # Replace the original file with the modified version
-            os.replace(temp_file, config_file)
-            
-            # Reload the Wi-Fi configuration
-            os.system('sudo wpa_cli -i wlan0 reconfigure')
-            
-            print(f"Successfully updated Wi-Fi connection to {name}")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-    
-    def connectToWiFi(self):
+    def connectedToWiFi(self):
 
         # Get the current IP address
         result = subprocess.run(['hostname', '-I'], capture_output=True)
