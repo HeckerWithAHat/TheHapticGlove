25641ccf4d0766cf5115fb73e755ce22ff8aa7c3
HeckerWithAHat <aryanj1010@gmail.com>
2025-02-17 21:10:13 -0800

    update spotify app and remove stopwatch/time app

--- apps/SpotifyApp.py
+++ apps/SpotifyApp.py
@@ -2,81 +2,158 @@ from AppAPI import *
 from globalvars import *
 import  requests
 from time import sleep
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 class SpotifyApp:
-        
+
     def play_pause(self):
-        response = requests.get('https://gloveserver.heckerwithahat.dev/spotify/playpause')
-        screen.clear()
-        screen.move_to(0,0)
-        screen.putstr("Spotify")
-        screen.move_to(0,1)
-        screen.putstr(response.text)
-        sleep(1)
-        screen.clear()
+        devices = self.sp.devices()
+        active_device = next((device for device in devices['devices'] if device['is_active']), None)
+        if active_device is None:
+            print("No active device found.")
+            return "No Active Device"
+        try:
+            if self.sp.current_playback()['is_playing']:
+                # Pause playback
+                print("Pausing music...")
+                self.sp.pause_playback(device_id=active_device['id'])        
+                print(f"Toggled playback on device {active_device['name']}")
+                return "Paused"
+            else:
+                # Resume playback
+                print("Resuming music...")
+                self.sp.start_playback(device_id=active_device['id'])
+                print(f"Toggled playback on device {active_device['name']}")
+                return "Unpaused"
+        except Exception as e:
+            print(f"An error occurred while toggling playback: {e}")
+            return "error"
         
     def shuffle_toggle(self):
-        response = requests.get('https://gloveserver.heckerwithahat.dev/spotify/shuffletoggle')
-        screen.clear()
-        screen.move_to(0,0)
-        screen.putstr("Spotify")
-        screen.move_to(0,1)
-        screen.putstr(response.text)
-        sleep(1)
-        screen.clear()
+        devices = self.sp.devices()
+        active_device = next((device for device in devices['devices'] if device['is_active']), None)
+        if active_device is None:
+            print("No active device found.")
+            return "No Active Device"
+        try:
+            print(self.sp.current_playback())
+            if self.sp.current_playback()['shuffle_state']:
+                # Pause playback
+                print("Unshuffling music...")
+                self.sp.shuffle(False, device_id=active_device['id'])        
+                print(f"Unshuffled playback on device {active_device['name']}")
+                return "Unshuffled"
+            else:
+                # Resume playback
+                print("Shuffling music...")
+                self.sp.shuffle(True,device_id=active_device['id'])
+                print(f"Shuffled playback on device {active_device['name']}")
+                return "Shuffled"
+        except Exception as e:
+            print(f"An error occurred while toggling playback: {e}")
+            return "error"
         
     def repeat_toggle(self):
-        response = requests.get('https://gloveserver.heckerwithahat.dev/spotify/repeattoggle')
-        screen.clear()
-        screen.move_to(0,0)
-        screen.putstr("Spotify")
-        screen.move_to(0,1)
-        screen.putstr(response.text)
-        sleep(1)
-        screen.clear()
+        devices = self.sp.devices()
+        active_device = next((device for device in devices['devices'] if device['is_active']), None)
+        if active_device is None:
+            print("No active device found.")
+            return "No Active Device"
+        try:
+            if self.sp.current_playback()['repeat_state'] == 'context':
+                print("Not repeating music...")
+                self.sp.repeat("off", device_id=active_device['id'])        
+                print(f"Not repeating playback on device {active_device['name']}")
+                return "Repeat Off"
+            elif self.sp.current_playback()['repeat_state'] == 'track':
+                print("Repeat all music...")
+                self.sp.repeat("context",device_id=active_device['id'])
+                print(f"Repeat all playback on device {active_device['name']}")
+                return "Repeat All"
+            elif self.sp.current_playback()['repeat_state'] == 'off':
+                print("Repeat current music...")
+                self.sp.repeat("track",device_id=active_device['id'])
+                print(f"Repeat current playback on device {active_device['name']}")
+                return "Repeat once"
+        except Exception as e:
+            print(f"An error occurred while toggling playback: {e}")
+            return "error"
         
     def skip(self):
-        response = requests.get('https://gloveserver.heckerwithahat.dev/spotify/next')
-        screen.clear()
-        screen.move_to(0,0)
-        screen.putstr("Spotify")
-        screen.move_to(0,1)
-        screen.putstr(response.text)
-        sleep(1)
-        screen.clear()
+        devices = self.sp.devices()
+        active_device = next((device for device in devices['devices'] if device['is_active']), None)
+        if active_device is None:
+            print("No active device found.")
+            return "No Active Device"
+        try:
+            self.sp.next_track(device_id=active_device['id'])
+            print(f"Skipped track on device {active_device['name']}")
+            return "Skipped Track"
+        except Exception as e:
+            print(f"An error occurred while toggling playback: {e}")
+            return "error"
         
     def previous(self):
-        response = requests.get('https://gloveserver.heckerwithahat.dev/spotify/back')
-        screen.clear()
-        screen.move_to(0,0)
-        screen.putstr("Spotify")
-        screen.move_to(0,1)
-        screen.putstr(response.text)
-        sleep(1)
-        screen.clear()
+        devices = self.sp.devices()
+        active_device = next((device for device in devices['devices'] if device['is_active']), None)
+        if active_device is None:
+            print("No active device found.")
+            return "No Active Device"
+        try:
+            self.sp.previous_track(device_id=active_device['id'])
+            print(f"Back tracked on device {active_device['name']}")
+            return "Back Tracked"
+        except Exception as e:
+            print(f"An error occurred while toggling playback: {e}")
+            return "error"
         
     def volume_up(self):
-        response = requests.get('https://gloveserver.heckerwithahat.dev/spotify/volume/up')
-        screen.clear()
-        screen.move_to(0,0)
-        screen.putstr("Spotify")
-        screen.move_to(0,1)
-        screen.putstr(response.text)
-        sleep(1)
-        screen.clear()
+        devices = self.sp.devices()
+        active_device = next((device for device in devices['devices'] if device['is_active']), None)
+        if active_device is None:
+            print("No active device found.")
+            return "No Active Device"
+        try:
+            device_id = active_device['id']
+            current_volume = active_device['volume_percent']
+            self.sp.volume(device_id=device_id, volume_percent=current_volume+5)
+            print(f"Decreased volume from {current_volume}% to " + str(current_volume+5) + "%")
+            return "New Volume: " + str(current_volume+5)
+        except Exception as e:
+            print(f"Error managing volume: {e}")
+            return "error"
         
     def volume_down(self):
-        response = requests.get('https://gloveserver.heckerwithahat.dev/spotify/volume/down')
-        screen.clear()
-        screen.move_to(0,0)
-        screen.putstr("Spotify")
-        screen.move_to(0,1)
-        screen.putstr(response.text)
-        sleep(1)
-        screen.clear()
+        devices = self.sp.devices()
+        active_device = next((device for device in devices['devices'] if device['is_active']), None)
+        if active_device is None:
+            print("No active device found.")
+            return "No Active Device"
+        try:
+            device_id = active_device['id']
+            current_volume = active_device['volume_percent']
+            self.sp.volume(device_id=device_id, volume_percent=current_volume-5)
+            print(f"Decreased volume from {current_volume}% to " + str(current_volume-5) + "%")
+            return "New Volume: " + str(current_volume-5)
+        except Exception as e:
+            print(f"Error managing volume: {e}")
+            return "error"
         
     
     def __init__(self):
+        
+        self.spotify_client_id = "8f775a31bc9b4e67a8ae753400cd7cfb"
+        self.spotify_client_secret = "770936463c1a47068ef59d26f1ceb143"
+        self.scope='user-read-playback-state user-modify-playback-state user-read-currently-playing user-library-modify user-library-read'
+
+        self.auth_manager = SpotifyOAuth(client_id=self.spotify_client_id,
+                                client_secret=self.spotify_client_secret,
+                                redirect_uri='http://localhost:5000',
+                                scope=self.scope)
+        self.sp = spotipy.Spotify(client_credentials_manager=self.auth_manager)
+        
+        
         SpotifyApp = App("spotify", "Spotify")
         SpotifyApp.setAppDefinedCommands(
             playpause={"name": "Play/Pause", "method":self.play_pause},
