26e97e77a639d33457daed28c30be656fd58c7b5
HeckerWithAHat <aryanj1010@gmail.com>
2025-02-23 14:26:47 -0800

    commit

--- EPDAPI.py
+++ EPDAPI.py
@@ -27,9 +27,31 @@ def createImageFromButtons(appName, commandNames):
     imageToDisplay = Image.open('commandDisplayBase.jpg')  # 250*122
     drawblack = ImageDraw.Draw(imageToDisplay)
     drawblack.text((125, 15), appName, font = font20, fill = 0, anchor = 'mm', align = 'center')
-    for i in range(len(commandNames)):
-        drawblack.text((32+(62*(int(i/3))), 68+(20*(i&3))), commandNames[i], font = font18, fill = 0, anchor = 'mm', align = 'center')
+    for button in commandNames:
+        if button == "IT":
+            drawblack.text((32, 68), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "IM":
+            drawblack.text((32, 88), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "IB":
+            drawblack.text((32, 108), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "MT":
+            drawblack.text((94, 68), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "MM":
+            drawblack.text((94, 88), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "MB":
+            drawblack.text((94, 108), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "RT":
+            drawblack.text((156, 68), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "RM":
+            drawblack.text((156, 88), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "RB":
+            drawblack.text((156, 108), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "PT":
+            drawblack.text((220, 68), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
+        elif button == "PB":
+            drawblack.text((220, 108), commandNames[button], font = font18, fill = 0, anchor = 'mm', align = 'center')
     globalvars.epd.display(globalvars.epd.getbuffer(imageToDisplay))
+
     
     
 
